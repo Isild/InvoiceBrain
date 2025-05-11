79fe6118c4f65583bfb79bdc34f294c8823edbb7
Isild <pimok96@gmail.com>
2025-05-11 15:00:02 +0200

    add connection to celery

--- InvoiceBrain/settings.py
+++ InvoiceBrain/settings.py
@@ -139,3 +139,7 @@ ELASTICSEARCH_DSL = {
         "hosts": os.getenv("ELASTICSEARCH_HOST", "http://localhost:9200")
     }
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
